17ed2c471e8cf7920be6bcf908c93da04cd861c1
Emanuel Mendoza Mori <130607153+EmanuelMdz@users.noreply.github.com>
2023-08-09 19:55:52 -0300

    v1

--- knexfile.js
+++ knexfile.js
@@ -5,11 +5,11 @@ module.exports = {
   development: {
     client: "pg",
     connection: {
-      user: process.env.PGUSER,
-      password: process.env.PGPASSWORD,
-      host: process.env.PGHOST,
-      port: process.env.DB_PORT,
-      database: process.env.PGDATABASE,
+      user: "r4iko17",
+      password: "W5XucYDe6bFQ",
+      host: "ep-holy-breeze-31505336.us-east-2.aws.neon.tech",
+      port: 5432,
+      database: "danidb23",
       ssl: {
         rejectUnauthorized: true, // Configuración opcional para certificados autofirmados
       },
