81a528f3c863fccee64c46cc0b05acd532dec913
Emanuel Mendoza Mori <130607153+EmanuelMdz@users.noreply.github.com>
2023-08-09 20:11:20 -0300

    v1

--- knexfile.js
+++ knexfile.js
@@ -4,11 +4,11 @@ const path = require("path");
 module.exports = {
   client: "pg",
   connection: {
-    user: "r4iko17",
-    password: "W5XucYDe6bFQ",
-    host: "ep-holy-breeze-31505336.us-east-2.aws.neon.tech",
-    port: 5432,
-    database: "danidb23",
+    user: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    host: process.env.PGHOST,
+    port: process.env.DB_PORT,
+    database: process.env.PGDATABASE,
     ssl: {
       rejectUnauthorized: true, // Configuración opcional para certificados autofirmados
     },
@@ -16,6 +16,22 @@ module.exports = {
   migrations: {
     directory: path.join(__dirname, "database", "migrations"),
   },
-
-  // ...
 };
+// module.exports = {
+//   client: "pg",
+//   connection: {
+//     user: "r4iko17",
+//     password: "W5XucYDe6bFQ",
+//     host: "ep-holy-breeze-31505336.us-east-2.aws.neon.tech",
+//     port: 5432,
+//     database: "danidb23",
+//     ssl: {
+//       rejectUnauthorized: true, // Configuración opcional para certificados autofirmados
+//     },
+//   },
+//   migrations: {
+//     directory: path.join(__dirname, "database", "migrations"),
+//   },
+
+//   // ...
+// };
--- services/database.js
+++ services/database.js
@@ -3,7 +3,6 @@ require("dotenv").config();
 const knex = require("knex");
 const knexFile = require("../knexfile");
 
-const environment = process.env.NODE_ENV || "development";
 const db = knex(knexFile);
 
 // Define ANSI escape codes for colors
